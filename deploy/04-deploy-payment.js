6974ea593e1d48c71c146dcf2cf443c71ee14bc3
Kizito007 <nwakakelechi49@gmail.com>
2023-01-22 11:41:40 +0100

    added pricefeed address from chainId

--- deploy/04-deploy-payment.js
+++ deploy/04-deploy-payment.js
@@ -5,6 +5,7 @@ const { verify } = require("../utils/verify")
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
 
     let streamTokenAddress, vendorAddress, maticUsdPriceFeedAddress
 
